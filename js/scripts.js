72f2eb0c5ab5bc519f2e8132f4d9c78efb1a1230
DmitryTa <tskuadm@gmail.com>
2016-01-20 03:38:49 +0300

    mobile-menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
   $('.slider').slick({
-    dots: false
-
+    
+    infinite: true,
+    responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 1,
+        dots: true,
+        arrows: false
+      }
+    }
+  ]
   });
 });
